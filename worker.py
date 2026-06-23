7341fd9ff3e5fada026840fa6c2554fd9e0737bf
Athish06 <148446898+Athish06@users.noreply.github.com>
2026-06-24 01:28:17 +0530

    bug in worker.py solved

--- worker.py
+++ worker.py
@@ -5,13 +5,12 @@ from dotenv import load_dotenv
 # Load env variables before anything else
 load_dotenv()
 
-from rq import Worker, Queue, Connection
+from rq import Worker, Queue
 from services.redis_client import redis_conn
 
 listen = ['agent_tasks']
 
 if __name__ == '__main__':
-    with Connection(redis_conn):
-        worker = Worker(map(Queue, listen))
-        print("Starting RQ worker for agent tasks...")
-        worker.work()
+    worker = Worker(listen, connection=redis_conn)
+    print("Starting RQ worker for agent tasks...")
+    worker.work()
